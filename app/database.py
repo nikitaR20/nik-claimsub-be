e7e483f0406a92b243d792fb54e89fb0846e59de
Nikita Rathod <nikitarathod.d@gmail.com>
2025-06-25 22:57:56 -0500

    Made changes to database_url

--- app/database.py
+++ app/database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
 # Dummy DB connection string — update later
-DATABASE_URL = "postgresql://user:password@localhost/yourdb"
+DATABASE_URL = "postgresql://postgres:postgres@localhost/claims_db"
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(bind=engine, autocommit=False, autoflush=False)
